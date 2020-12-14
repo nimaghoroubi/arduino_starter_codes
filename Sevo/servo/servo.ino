ef95be940c0e7edd87fe3e39ec3a4db1ec81e4f8
Nima <nima.ghoroubi@gmail.com>
2020-12-14 22:24:07 +0100

    I have no idea what I am committing

--- Sevo/servo/servo.ino
+++ Sevo/servo/servo.ino
@@ -4,7 +4,7 @@
 
  modified 8 Nov 2013
  by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Sweep
+ http://www.arduino.cc/en/Tutorial/Sweepu
 */
 
 #include <Servo.h>
@@ -13,20 +13,28 @@ Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
 int pos = 0;    // variable to store the servo position
+int max = 180;
+int max_delay = 0;
 
 void setup() {
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 }
 
+// void loop (){
+//   myservo.write(pos);
+// }
+
 void loop() {
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+  for (pos = 0; pos <= max; pos += 6) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    pos == 180 ? delay(0):delay(1000);                       // waits 15ms for the servo to reach the position
   }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+  delay(max_delay);
+  for (pos = max; pos >= 0; pos -= 6) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    pos == 0 ? delay(0):delay(1000);                       // waits 15ms for the servo to reach the position
   }
+  delay(max_delay);
 }
 
